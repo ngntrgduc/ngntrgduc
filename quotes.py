90ce520b13c43720484b3b18e244804f22d9e0db
ngntrgduc <trungducnguyen1511@gmail.com>
2024-03-29 17:49:42 +0700

    Update fun fact

--- quotes.py
+++ quotes.py
@@ -7,12 +7,9 @@ CONTENT = \
   <img align="right" src="https://github.com/ngntrgduc/github-stats/blob/master/generated/overview.svg"/>
 </a>
 
-- 📖 I'm an ambivert junior at the University of Science - VNUHCM, majoring in Mathematics and Computer Science
-- 🧪 I make [random stuff](https://github.com/ngntrgduc/projects), [blog](https://ngntrgduc.github.io/) whenever I have no deadline
-- 🐧 Fun fact: I love listening to [music](https://soundcloud.com/ngntrgduc), sipping coffee, and making 🐛🐛🐛
-- 📫 Connect with me: [Facebook](https://fb.com/nguyenduc1511) | 
-[LinkedIn](https://www.linkedin.com/in/ngntrgduc/) | 
-[Email](mailto:trungducnguyen1511@gmail.com)
+- 📖 I'm a junior at the University of Science - VNUHCM, majoring in Mathematics and Computer Science.
+- 🧪 I make [random stuff](https://github.com/ngntrgduc/projects), [blog](https://ngntrgduc.github.io/) whenever I have no deadline (rarely).
+- 🐧 Fun fact: ISFJ-A. Ambivert. Sleep-deprived. Xiangqi player. Music. Coffee.
 """
 
 url = 'https://raw.githubusercontent.com/ngntrgduc/ngntrgduc.github.io/master/content/quotes.md'
