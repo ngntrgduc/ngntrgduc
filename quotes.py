33961e2876ead2c19339bb841ab0ee95f0905aa5
ngntrgduc <trungducnguyen1511@gmail.com>
2024-03-29 17:56:14 +0700

    Remove bullets

--- quotes.py
+++ quotes.py
@@ -4,12 +4,14 @@ import random
 CONTENT = \
 """
 <a href="https://github.com/ngntrgduc/github-stats">
-  <img align="left" src="https://github.com/ngntrgduc/github-stats/blob/master/generated/overview.svg"/>
+  <img align="right" src="https://github.com/ngntrgduc/github-stats/blob/master/generated/overview.svg"/>
 </a>
 
-- 📖 I'm a junior at the University of Science - VNUHCM, majoring in Mathematics and Computer Science.
-- 🧪 I make [random stuff](https://github.com/ngntrgduc/projects), [blog](https://ngntrgduc.github.io/) whenever I have no deadline (rarely).
-- 🐧 Fun fact: ISFJ-A. Ambivert. Sleep-deprived. Xiangqi player. Music. Coffee.
+📖 I'm a junior at the University of Science - VNUHCM, majoring in Mathematics and Computer Science.
+
+🧪 I make [random stuff](https://github.com/ngntrgduc/projects), [blog](https://ngntrgduc.github.io/) whenever I have no deadline (rarely).
+
+🐧 Fun fact: ISFJ-A. Ambivert. Sleep-deprived. Xiangqi player. Music. Coffee.
 """
 
 url = 'https://raw.githubusercontent.com/ngntrgduc/ngntrgduc.github.io/master/content/quotes.md'
